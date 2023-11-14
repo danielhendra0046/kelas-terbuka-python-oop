bdadb50f1a081e554044fe8515252d22e4554048
danielhendra0046 <danielhendra08@gmail.com>
2023-11-14 12:21:03 +0700

    add new lessons

---
+++ Main.py
@@ -0,0 +1,13 @@
+from Hero import HeroIntelligent,HeroStrength
+
+lina = HeroIntelligent('lina')
+slardar = HeroStrength('slardar')
+
+lina.show_Info()
+slardar.show_Info()
+
+lina.gainExp = 200
+slardar.gainExp = 250
+
+lina.show_Info()
+slardar.show_Info()
